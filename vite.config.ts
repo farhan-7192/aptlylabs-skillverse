6528eb5234d937e8c4a938be20c992369573fa7c
Farhan <farhan@aptlylabs.com>
2026-05-19 14:46:47 +0530

    [Chore]: Implemented alias for components and utils

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const rootDir = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(rootDir, 'src'),
+      },
+      {
+        find: '@components',
+        replacement: path.resolve(rootDir, 'src/components/common'),
+      },
+      {
+        find: '@utils',
+        replacement: path.resolve(rootDir, 'src/lib'),
+      },
+    ],
+  },
 })
